717746b139a8913d779694da1ff29a0d357b4efd
edward ciccone <edward.ciccone@365.elmhurst.edu>
2016-10-07 11:35:11 -0500

    updated comments

--- src/project3/Project3.java
+++ src/project3/Project3.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3;
 
 import java.io.File;
@@ -25,13 +20,13 @@ public class Project3 {
      * Creates Scanner object, and opens Scanner to get input from user.
      */
     public static Scanner input = new Scanner(System.in);
-    
+
     /**
-     * Creates Scanner object, and initializes to null, used to scan a
-     * file for a specific name.
+     * Creates Scanner object, and initializes to null, used to scan a file for
+     * a specific name.
      */
     public static Scanner fileScanner = null;
-    
+
     /**
      * The File object that is scanned for a specific name.
      */
@@ -40,7 +35,7 @@ public class Project3 {
     /**
      * Asks the user for the year, gender, and name, and then attempts to find
      * the name.
-     * 
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -63,7 +58,7 @@ public class Project3 {
 
     /**
      * Asks the user for the baby name to look for and then returns this name.
-     * 
+     *
      * @return the baby name to look up
      */
     private static String setAndGetName() {
@@ -74,7 +69,7 @@ public class Project3 {
 
     /**
      * Asks the user for the gender of the baby and then returns it.
-     * 
+     *
      * @return the baby gender
      * @throws IOException if gender doesn't equal m or f, throws IOException.
      */
@@ -89,17 +84,19 @@ public class Project3 {
 
     /**
      * Asks the user for the year of the records and then returns it.
-     * 
+     *
      * @return the year of the record
-     * @throws FileNotFoundException if no record for that year is found, throws FileNotFoundException
-     * @throws InputMismatchException if year is not entered as an integer, throws InputMismatchException
+     * @throws FileNotFoundException  if no record for that year is found,
+     *                                throws FileNotFoundException
+     * @throws InputMismatchException if year is not entered as an integer,
+     *                                throws InputMismatchException
      */
     private static int setAndGetYear() throws FileNotFoundException, InputMismatchException {
         System.out.println("Enter the year: ");
         int year = input.nextInt();
         file = new File("Babynamesranking" + year + ".txt");
+        /* Opens scanner to the name records if file exists, otherwise throws a FileNotFoundException. */
         if (file.exists()) {
-            /* Opens scanner to the name records. */
             fileScanner = new Scanner(file);
         } else {
             throw new FileNotFoundException("No records for year " + year + " were found.");
@@ -109,11 +106,11 @@ public class Project3 {
 
     /**
      * Uses a Scanner for the appropriate file record, and searches for the name
-     * of the baby; male names and female names are searched differently, so 
-     * the name is dependent on the gender.
-     * 
-     * @param name the name to search for
-     * @param year the year of the record
+     * of the baby; male names and female names are searched differently, so the
+     * name is dependent on the gender.
+     *
+     * @param name   the name to search for
+     * @param year   the year of the record
      * @param gender the gender of the baby
      */
     private static void findName(String name, int year, String gender) {
@@ -126,15 +123,15 @@ public class Project3 {
             /* Rank of the name is the first element for both males and females. */
             int rank = fileScanner.nextInt();
             String currentName = "";
-            
+
             /* Male names appear as the second element */
             if (male) {
                 currentName = fileScanner.next();
                 fileScanner.next();
                 fileScanner.next();
                 fileScanner.next();
-            } 
-            
+            }
+
             /* Female names appear as the fourht element. */
             if (female) {
                 fileScanner.next();
@@ -142,13 +139,13 @@ public class Project3 {
                 currentName = fileScanner.next();
                 fileScanner.next();
             }
-            
+
             /* If the name was found the rank is displayed to the user. */
             if (currentName.equalsIgnoreCase(name)) {
                 System.out.println(currentName + " is ranked #" + rank + " in year " + year);
                 break;
             }
-            
+
             /* If the name was not found a message is sent to the user. */
             if (!fileScanner.hasNext()) {
                 System.out.println("The name " + name + " is not ranked in year " + year);
